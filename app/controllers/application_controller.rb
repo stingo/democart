2709708ad2669feafbe1f0de1c6bb87096028a6e
sakader <rkj.snake@gmail.com>
2020-11-17 17:03:02 +0600

    Fixed after login view issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 	include ApplicationHelper
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_location, unless: :devise_controller?
   before_action :set_order_items
 
 
@@ -18,10 +19,29 @@ class ApplicationController < ActionController::Base
   end
 
 
+  def after_sign_in_path_for(resource)
+    previous_path = session[:previous_url]
+    session[:previous_url] = nil
+    previous_path || root_path
+  end
+
+
   private
 
   def set_order_items
     @order_items = current_ordering.order_items
+  end
+
+  def store_location
+    if(request.path != "/profiles/sign_in" &&
+        request.path != "/profiles/sign_up" &&
+        request.path != "/profiles/password/new" &&
+        request.path != "/profiles/password/edit" &&
+        request.path != "/profiles/confirmation" &&
+        request.path != "/uprofiles/sign_out" &&
+        !request.xhr? && !current_profile) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
   end
 
 
